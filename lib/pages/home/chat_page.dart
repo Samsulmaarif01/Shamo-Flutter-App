9f67db25a468b0121e88b8636a33435a5922178e
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-08-19 11:53:08 +0000

    feat(chat_page): Add Content

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('chat page', style: primaryTextStyle.copyWith(fontSize: 20),),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/Headset.png',
+                width: 80,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opss no message yet?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'You have never done a transaction',
+                style: secondaryTextStyle,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 44,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 10,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
