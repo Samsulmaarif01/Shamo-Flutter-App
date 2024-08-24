a48feb932bbceb9b52ebdf0095c26d83592288f5
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2024-08-24 08:38:30 +0000

    add(profile_page.dart): AppBar Profile

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,65 @@
+
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+
 class ProfilePage extends StatelessWidget {
+
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile page', style: primaryTextStyle.copyWith(fontSize: 20),),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+
+          child: Container(
+            padding: const EdgeInsets.all(defaultMargin),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/Group 1347.png',
+                    width: 64,
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Arif',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 24,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@samsulmaarif',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset('assets/Exit Button.png', width: 20,)
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
